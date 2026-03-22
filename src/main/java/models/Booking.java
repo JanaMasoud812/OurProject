9ea89467a7d835c00514eff6fd414cd08f8da6ab
Fawzia Orade <fawzia@gmail.com>
2026-03-22 23:59:15 +0200

    new commit fawzia

---
+++ src/main/java/models/Booking.java
@@ -0,0 +1,85 @@
+package models;
+
+public class Booking {
+	
+	private String username;
+	private String time;
+	private String status;
+	private int duration;
+	private int participants;
+	
+	
+	public String getUsername()
+	{ return username;}
+	public void setUsername(String username)
+	{ this.username= username;}
+	
+	
+	public String getTime()
+	{ return time;}
+	public void setTime(String time)
+	{ this.time= time;}
+	
+	
+	public String getStatus()
+	{ return status;}
+	public void setStatus(String status)
+	{ this.status= status;}
+	
+	public int getDuration()
+	{ return duration;}
+	public void setDuration(int duration)
+	{ this.duration= duration;}
+	
+	
+	public int getParticipants()
+	{ return participants;}
+	public void setParticipants(int participants)
+	{ this.participants= participants;}
+	
+	
+	public Booking( String username , String time , String status , int duration , int participants)
+	{
+		this.username= username;
+		this.time = time;
+		this.status= "Confirmed";
+		this.duration=  duration;
+		this.participants=participants;
+		
+	}
+	
+	public void confirmBooking()
+	{
+		this.status="Confirmed";
+		
+	}
+	
+	
+	public void cancelBooking()
+	{
+		this.status="canceled";
+	}
+	
+	
+	public boolean isFull(int maxParticipants)
+	{
+		return participants >= maxParticipants;
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
